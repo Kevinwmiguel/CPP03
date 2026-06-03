d9bb7544c2942401f9b2cbd79fbd7d0c824e554f
Kevinwmiguel <kevincursos@outlook.com>
2026-06-04 00:25:53 +0100

    ClapTrap

---
+++ ex01/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kwillian <kwillian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/03 23:40:44 by kwillian          #+#    #+#             */
+/*   Updated: 2026/06/04 00:20:05 by kwillian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int main()
+{
+    ClapTrap Clap("Clap");
+    ClapTrap Clap2("Trap");
+    
+    Clap.attack("Trap");
+    Clap2.beRepaired(3);
+    
+    Clap2.attack("Clap");
+    Clap.takeDamage(999);
+
+    Clap2.beRepaired(4);
+    
+
+    return (0);
+}
